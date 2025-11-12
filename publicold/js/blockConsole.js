1aefdfa735a535e99f872570a416299b3e6d1c7b
DESKTOP-KNF94FO\Micky <tientrinhpanda@gmail.com>
2025-11-12 22:45:13 +0700

    Remove Public old

--- publicold/js/blockConsole.js
+++
@@ -1,5 +0,0 @@
-if(!window.console) window.console = {};
-var methods = ["log", "debug", "warn", "info", "dir", "dirxml", "trace", "profile"];
-for(var i=0;i<methods.length;i++){
-    console[methods[i]] = function(){};
-}
